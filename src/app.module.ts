1f0d01708ccf1f6d434253a4a483ce6b01a700c5
Bryan-E-Flores-M <bryanfloresmejia7@gmail.com>
2026-04-14 18:29:59 -0400

    Creando estructura de modulo estudiante

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EstudiantesControllersController } from './modules/estudiantes/controllers/estudiantes-controllers.controller';
+import { EstudianteService } from './modules/estudiantes/services/estudiante.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, EstudiantesControllersController],
+  providers: [AppService, EstudianteService],
 })
 export class AppModule {}
---
+++ src/modules/estudiantes/controllers/estudiantes-controllers.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('estudiantes-controllers')
+export class EstudiantesControllersController {}
