5f60cd4f5a6e2f1f23b6b49b24278f97fafba297
Bryan-E-Flores-M <bryanfloresmejia7@gmail.com>
2026-04-19 15:33:42 -0400

    agregandom dependencias y pequenios cambios en students.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { StudentsModule } from './modules/students/students.module';
 
-
 @Module({
-  imports: [StudentsModule]
-  
+  imports: [StudentsModule],
 })
 export class AppModule {}
