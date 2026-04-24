61f4f86f7113ad7d29ae071fbed18978f843f15e
Bryan-E-Flores-M <bryanfloresmejia7@gmail.com>
2026-04-24 14:01:12 -0400

    agregando cambios a app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudentsModule } from './modules/students/students.module';
+import { ExitPermitsController } from './modules/exit_permits/controllers/exit_permits.controller';
+import { ExitPermitsService } from './modules/exit_permits/services/exit_permits.service';
 
 @Module({
   imports: [StudentsModule],
+  controllers: [ExitPermitsController],
+  providers: [ExitPermitsService],
 })
 export class AppModule {}
