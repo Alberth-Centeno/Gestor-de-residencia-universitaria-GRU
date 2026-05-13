5ebe4e9a2486e6238318ec7f127e38eb648f0d81
Rodrigo Báez <168125189+brodr1@users.noreply.github.com>
2026-05-13 14:40:30 -0600

    dto en proceso

---
+++ src/modules/feedbacks/dto/feedback.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import {IsInt,IsNotEmpty,IsNumber,IsOptional,IsPositive,IsString,MinLength,} from 'class-validator'
+
+export class CreateFeedbackdto {
+
+
+    
+
+
+}
