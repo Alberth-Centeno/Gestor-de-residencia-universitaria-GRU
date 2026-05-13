2dae660b1d7474a59e9feeb412c482fbfe1b668d
Rodrigo Báez <168125189+brodr1@users.noreply.github.com>
2026-05-13 15:10:43 -0600

    feat: agregamos al modulo feedback dto

--- src/modules/feedbacks/dto/feedback.dto.ts
+++ src/modules/feedbacks/dto/feedback.dto.ts
@@ -1,10 +1,31 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
-import {IsInt,IsNotEmpty,IsNumber,IsOptional,IsPositive,IsString,MinLength,} from 'class-validator'
+import {IsInt,IsNotEmpty,IsNumber,IsOptional,IsPositive,IsString,MinLength,IsBoolean} from 'class-validator'
 
-export class CreateFeedbackdto {
+export class CreateFeedbackDto {
 
+    @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty()
+  student_id!: number; 
 
-    
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @ApiProperty()
+  type!: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @ApiProperty()
+  content!: string;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty()
+  is_read!: boolean;
 
 }
+export class UpdateFeedbackDto extends PartialType(CreateFeedbackDto) {}
