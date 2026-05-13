5ec20ede71a6972e1661cf62b74414e9208f4d54
Alberth-Centeno <alberththegabrile@gmail.com>
2026-05-13 17:57:53 -0600

    agregando controller y dto al modulo students

--- src/modules/students/dto/student.dto.ts
+++ src/modules/students/dto/student.dto.ts
@@ -1,1 +1,50 @@
-export class StudentDto {}
+import { IsEmail, IsNotEmpty, IsString, IsEnum, IsOptional, MinLength } from 'class-validator';
+import { UserRole } from '../../users/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateStudentDto {
+  // --- Datos para la cuenta de Usuario ---
+  @IsEmail({}, { message: 'El correo electrónico no es válido' })
+  @IsNotEmpty()
+  @ApiProperty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
+  password: string;
+
+  @IsEnum(UserRole)
+  @IsOptional()
+  @ApiProperty()
+  role?: UserRole = UserRole.STUDENT; 
+
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  student_code: string; 
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  first_name: string; 
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  last_name: string; 
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  career: string; 
+
+  @IsString()
+  @IsOptional()
+  room_number?: string; 
+
+  @IsString()
+  @IsOptional()
+  scholarship_status?: string = 'Active'; 
+}
