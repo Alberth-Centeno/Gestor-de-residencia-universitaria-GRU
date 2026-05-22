07892886696e2088835438d5a92563d1272a6edb
Pedro-Urbina-18 <pedrosolorzano777@gmail.com>
2026-05-21 22:23:12 -0600

    feat: inyectar student service en pases de salida y corregir con forwardRef

--- src/modules/exit_permits/exit_permits.module.ts
+++ src/modules/exit_permits/exit_permits.module.ts
@@ -1,12 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ExitPermitsService } from './services/exit_permits.service';
 import { ExitPermitsController } from './controllers/exit_permits.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExitPermit } from './entities/exit_permits.entity';
+import { StudentsModule } from '../students/students.module'; 
 @Module({
-  imports: [TypeOrmModule.forFeature([ExitPermit])],
-  exports: [TypeOrmModule, ExitPermitsService],
-  providers: [ExitPermitsService],
-  controllers: [ExitPermitsController],
+  imports: [
+    TypeOrmModule.forFeature([ExitPermit]),
+    forwardRef(() => StudentsModule), // <-- Solución al error: Envolvemos el módulo con forwardRef
+  ],
+  exports: [
+    TypeOrmModule, 
+    ExitPermitsService
+  ],
+  providers: [
+    ExitPermitsService
+  ],
+  controllers: [
+    ExitPermitsController
+  ],
 })
 export class ExitPermitsModule {}
